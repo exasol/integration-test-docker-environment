45d0d6add1fcc1cc559db750a6e629184a0b9764
Torsten Kilias <torsten.kilias@exasol.com>
2019-06-25 10:35:23 +0200

    fix missing parameter in build_config and move cd "$SCRIPT_DIR" to the start of the pipenv initialisation in start-test-env.sh

--- docker_db_starter_src/lib/build_config.py
+++ docker_db_starter_src/lib/build_config.py
@@ -4,4 +4,4 @@ import luigi
 class build_config(luigi.Config):
     temporary_base_directory = luigi.OptionalParameter(None)
     output_directory = luigi.Parameter(".build_output")
-
+    log_build_context_content = luigi.BoolParameter(False)
