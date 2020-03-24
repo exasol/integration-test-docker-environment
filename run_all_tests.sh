aa35b36d2b225e9142f4f7826ce407758740aada
Torsten Kilias <torsten.kilias@exasol.com>
2020-03-24 13:34:33 +0100

    Extract tests from exaslct and adapt travis config

---
+++ run_all_tests.sh
@@ -0,0 +1,18 @@
+#!/bin/bash 
+   
+COMMAND_LINE_ARGS=$* 
+SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")" 
+ 
+source pipenv_utils.sh 
+ 
+discover_pipenv 
+init_pipenv "$PIPENV_BIN" 
+
+if [ -n "$PIPENV_BIN" ]
+then
+  PYTHONPATH=. $PIPENV_BIN run python3 -m unittest discover src/test
+else
+  echo "Could not find pipenv!"
+  exit 1
+fi
+
