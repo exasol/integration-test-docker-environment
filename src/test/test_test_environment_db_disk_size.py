f0ae21097a870e66d51cc8e7776b710cb3c5bb73
Torsten Kilias <torsten.kilias@exasol.com>
2020-06-09 11:22:02 +0200

    Fix test_test_environment_db_disk_size.py

--- src/test/test_test_environment_db_disk_size.py
+++ src/test/test_test_environment_db_disk_size.py
@@ -37,7 +37,7 @@ class DockerTestEnvironmentDBDiskSizeTest(unittest.TestCase):
     def assert_disk_size(self, size:str):
         containers = [c.name for c in self.client.containers.list() if self.docker_environment_name in c.name]
         db_container = [c for c in containers if "db_container" in c]
-        exit_result = self.client.containers.get(db_container[0]).exec_run("cat /exa/etc/EXAConf | grep MemSize")
+        exit_result = self.client.containers.get(db_container[0]).exec_run("cat /exa/etc/EXAConf | grep Size")
         output = exit_result[1].decode("UTF-8")
         self.assertIn(" Size = %s"%size,output)
 
