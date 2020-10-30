bc7bbac14ca49480be56709e02580006db2aefcf
Torsten Kilias <torsten.kilias@exasol.com>
2020-10-30 11:46:52 +0100

    Extract setup.py from tar-package build by poetry in githooks/update_packaging.sh

--- githooks/update_packaging.sh
+++ githooks/update_packaging.sh
@@ -18,8 +18,17 @@ pushd "$ROOT_DIR" > /dev/null
 
 rm -r exasol_integration_test_docker_environment/docker_db_config 
 cp -rL docker_db_config_template exasol_integration_test_docker_environment/docker_db_config
+rm -r dist
+poetry build
+pushd dist
+tar_file=$(ls *.tar.gz)
+extracted_dir=${tar_file%.tar.gz}
+tar -xf $tar_file
+cp "$extracted_dir/setup.py" ../setup.py
+rm -r "$extracted_dir"
+popd
 dephell deps convert
 sed -i 's/python_version = "3.6"//g' Pipfile
-git add Pipfile
+git add Pipfile setup.py
 
 popd > /dev/null
