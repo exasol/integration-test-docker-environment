f67cd0dca737dee0ae096e2b7786477b3657166f
Torsten Kilias <tkilias@users.noreply.github.com>
2021-09-08 13:36:42 +0200

    Format task states
    Co-authored-by: Thomas Ubensee <34603111+tomuben@users.noreply.github.com>

--- exasol_integration_test_docker_environment/lib/base/task_state.py
+++ exasol_integration_test_docker_environment/lib/base/task_state.py
@@ -4,9 +4,9 @@ from enum import Enum
 class TaskState(Enum):
     NONE = 0,
     INIT = 1,
-    AFTER_INIT =6,
-    RUN = 2,
-    FINISHED = 3,
-    CLEANUP = 4,
-    CLEANED = 5
+    AFTER_INIT =2,
+    RUN = 3,
+    FINISHED = 4,
+    CLEANUP = 5,
+    CLEANED = 6,
     ERROR = 7
