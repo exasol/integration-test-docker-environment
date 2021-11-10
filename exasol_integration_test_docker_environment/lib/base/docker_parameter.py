a963548eb3cc2633b2ff4c72e8d72640bb4dd908
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2021-11-10 06:42:33 -0300

    Added missing file docker_parameter.py

---
+++ exasol_integration_test_docker_environment/lib/base/docker_parameter.py
@@ -0,0 +1,12 @@
+import luigi
+from luigi import Config
+from luigi.parameter import ParameterVisibility
+
+
+class DockerParameter(Config):
+    """
+    Docker parameters used for Tasks accessing Docker client.
+    """
+
+    timeout = luigi.IntParameter(100000, significant=False, visibility=ParameterVisibility.PRIVATE)
+    no_cache = luigi.BoolParameter(False)
