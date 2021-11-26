0c16b6d0aadc67ed9bba05fa4e83c55d16602a18
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2021-11-26 12:13:00 -0300

    #25: Cleanup methods can be called multiple times

--- exasol_integration_test_docker_environment/test/test_base_task_cleanup.py
+++ exasol_integration_test_docker_environment/test/test_base_task_cleanup.py
@@ -2,7 +2,7 @@ import shutil
 import unittest
 
 import luigi
-from luigi import Parameter
+from luigi import BoolParameter, IntParameter
 
 from exasol_integration_test_docker_environment.cli.common import generate_root_task
 from exasol_integration_test_docker_environment.lib.base.dependency_logger_base_task import DependencyLoggerBaseTask
@@ -11,56 +11,131 @@ TestBaseTask = DependencyLoggerBaseTask
 
 
 class TestTaskBase(TestBaseTask):
+    different_grandchild = BoolParameter()
+    use_dynamic_dependency = BoolParameter()
+
+    def _build_child_task(self, use_dynamic_dependency):
+        index_for_grandchild = 0
+        child_a = self.create_child_task(task_class=TestTaskChildA,
+                                         index_for_grandchild=index_for_grandchild,
+                                         use_dynamic_dependency=use_dynamic_dependency)
+        if self.different_grandchild:
+            index_for_grandchild = 1
+        child_b = self.create_child_task(task_class=TestTaskChildB,
+                                         index_for_grandchild=index_for_grandchild,
+                                         use_dynamic_dependency=use_dynamic_dependency)
+        return child_a, child_b
+
     def register_required(self):
-        grandchild = self.create_child_task(task_class=TestTaskGrandchild)
-        childA = self.create_child_task(task_class=TestTaskChild, grandchild=grandchild, index=0)
-        childB = self.create_child_task(task_class=TestTaskChild, grandchild=grandchild, index=1)
-        self.child_tasks = self.register_dependencies([childA, childB])
+        if not self.use_dynamic_dependency:
+            child_a, child_b = self._build_child_task(False)
+            self.child_tasks = self.register_dependencies([child_a, child_b])
 
     def run_task(self):
-        self.logger.info(f"GREMLING: TestTaskBase started")
+        if self.use_dynamic_dependency:
+            child_a, child_b = self._build_child_task(True)
+            yield from self.run_dependencies([child_a, child_b])
 
 
-class TestTaskChild(TestBaseTask):
-    grandchild = Parameter()
-    index = Parameter()
+class TestTaskChildA(TestBaseTask):
+    index_for_grandchild = IntParameter()
+    use_dynamic_dependency = BoolParameter()
 
     def register_required(self):
-        self.register_dependency(self.grandchild)
+        if not self.use_dynamic_dependency:
+            grandchild = self.create_child_task(task_class=TestTaskGrandchild,
+                                                index_for_grandchild=self.index_for_grandchild)
+            self.register_dependency(grandchild)
 
     def run_task(self):
-        self.logger.info(f"GREMLING: TestTaskChild started {self.index}")
+        if self.use_dynamic_dependency:
+            grandchild = self.create_child_task(task_class=TestTaskGrandchild,
+                                                index_for_grandchild=self.index_for_grandchild)
+            yield from self.run_dependencies(grandchild)
 
-    def cleanup_task(self, success:bool):
-        self.logger.info(f"GREMLING: Cleanup of TestTaskChild: {self.index}")
+    def cleanup_task(self, success: bool):
+        pass
+
+
+class TestTaskChildB(TestBaseTask):
+    index_for_grandchild = IntParameter()
+    use_dynamic_dependency = BoolParameter()
+
+    def register_required(self):
+        if not self.use_dynamic_dependency:
+            grandchild = self.create_child_task(task_class=TestTaskGrandchild,
+                                                index_for_grandchild=self.index_for_grandchild)
+            self.register_dependency(grandchild)
+
+    def run_task(self):
+        if self.use_dynamic_dependency:
+            grandchild = self.create_child_task(task_class=TestTaskGrandchild,
+                                                index_for_grandchild=self.index_for_grandchild)
+            yield from self.run_dependencies(grandchild)
+
+    def cleanup_task(self, success: bool):
+        pass
+
+
+global_counter = 0
 
 
 class TestTaskGrandchild(TestBaseTask):
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.cleanup_counter = 0
+    index_for_grandchild = IntParameter()
 
     def run_task(self):
-        self.logger.info(f"GREMLING: TestTaskGrandchild started")
+        pass
 
-    def cleanup_task(self, success:bool):
-        self.logger.info(f"GREMLING: Cleanup")
-        self.cleanup_counter = self.cleanup_counter + 1
-        if self.cleanup_counter > 1:
-            raise RuntimeError("Cleanup must be called only once")
+    def cleanup_task(self, success: bool):
+        global global_counter
+        global_counter = global_counter + 1
 
 
 class BaseTaskCleanupTest(unittest.TestCase):
 
-    def test_cleanup_of_grandchildren_called_only_once(self):
-        task = generate_root_task(task_class=TestTaskBase)
+    def _run_it(self, different_grandchild, use_dynamic_dependency, expected_result):
+        global global_counter
+        global_counter = 0
+        task = generate_root_task(task_class=TestTaskBase,
+                                  different_grandchild=different_grandchild,
+                                  use_dynamic_dependency=use_dynamic_dependency)
         try:
             luigi.build([task], workers=3, local_scheduler=True, log_level="INFO")
             task.cleanup(success=True)
         finally:
             if task._get_tmp_path_for_job().exists():
                 shutil.rmtree(str(task._get_tmp_path_for_job()))
+        self.assertEqual(global_counter, expected_result, "number of Cleanups not matching")
+
+    def test_cleanup_of_grandchildren_called_only_once(self):
+        """
+        Test that creating the same grandchild task by two different parent task, will invokes cleanup of grandchild
+        task only once! Luigi takes care of invoking run only once, we take care to invoke cleanup() only once.
+        """
+        self._run_it(False, False, 1)
+
+    def test_cleanup_of_grandchildren_called_twice(self):
+        """
+        Test that creating grandchild task with different parameters by two different parent task,
+        will invokes cleanup of grandchild twice.
+        """
+        self._run_it(True, False, 2)
+
+    def test_cleanup_of_grandchildren_called_only_once_dynamic(self):
+        """
+        Test that creating the same grandchild task by two different parent task, will invokes cleanup of grandchild
+        task only once! Luigi takes care of invoking run only once, we take care to invoke cleanup() only once.
+        In this test all child tasks are created dynamically.
+        """
+        self._run_it(False, True, 1)
+
+    def test_cleanup_of_grandchildren_called_twice_dynamic(self):
+        """
+        Test that creating grandchild task with different parameters by two different parent task,
+        will invokes cleanup of grandchild twice.
+        In this test all child tasks are created dynamically.
+        """
+        self._run_it(True, True, 2)
 
 
 if __name__ == '__main__':
