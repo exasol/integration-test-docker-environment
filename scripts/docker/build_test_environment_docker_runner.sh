8264c151905386c38d3c93246d7bf0a984518a39
Torsten Kilias <torsten.kilias@exasol.com>
2021-12-08 10:44:24 +0100

    Remove usage of old starter and docker scripts

--- scripts/docker/build_test_environment_docker_runner.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
-
-IMAGE_NAME=$("$SCRIPT_DIR/construct_docker_runner_image_name.sh")
-
-docker build -t "$IMAGE_NAME" "$SCRIPT_DIR/../.."
