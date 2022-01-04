d6582c3d16808ad388e96d3425be5b51a1fa7b96
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2022-01-04 06:35:51 -0300

    1. Fixed bug in build_test_environment_docker_runner.sh 2. Changed shellcheck.sh so that it runs on all scripts, but exits with error if any error occured.

--- scripts/build/shellcheck.sh
+++ scripts/build/shellcheck.sh
@@ -5,9 +5,11 @@ set -euo pipefail
 interesting_paths=("scripts" "docker_db_config_template" "exasol_integration_test_docker_environment" "githooks")
 
 SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+status=0
 
 for path in "${interesting_paths[@]}"; do
   find "$SCRIPT_DIR/../../$path" -name '*.sh' -type f -print0 | xargs -0 -n1 shellcheck -x
+  test $? -ne 0 && status=1
 done
 
 
@@ -15,4 +17,7 @@ interesting_files=("./start-test-env" "./start-test-env-with-poetry" "./start-te
 
 for f in "${interesting_files[@]}"; do
   shellcheck -x "$SCRIPT_DIR/../../$f"
+  test $? -ne 0 && status=1
 done
+
+exit "$status"
