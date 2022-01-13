23309b22bb740e167e553b28aef31174b0fbef71
Torsten Kilias <torsten.kilias@exasol.com>
2022-01-13 18:43:12 +0100

    Fix shellcheck.sh and fix shellcheck warnings in create_checksums.sh

--- githooks/create_checksums.sh
+++ githooks/create_checksums.sh
@@ -6,4 +6,4 @@ CHECKSUM_DIRECTORY="$PWD/checksums"
 if [ ! -e "$CHECKSUM_DIRECTORY" ]; then
   mkdir "$CHECKSUM_DIRECTORY"
 fi
-find $PWD -maxdepth 1 -type f -printf "%f\\0" | xargs --null -n1 -I{} bash -c "sha512sum {} > '$CHECKSUM_DIRECTORY/{}.sha512sum'"
+find "$PWD" -maxdepth 1 -type f -printf "%f\\0" | xargs --null -n1 -I{} bash -c "sha512sum {} > '$CHECKSUM_DIRECTORY/{}.sha512sum'"
