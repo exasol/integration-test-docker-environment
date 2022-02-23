9b3ff85b0c0de07a045a8f389aefb84b435dee86
Nicola Coretti <nico.coretti@gmail.com>
2022-02-23 09:32:51 +0100

    Update scripts/health_test.sh
    Co-authored-by: Thomas Ubensee <34603111+tomuben@users.noreply.github.com>

--- scripts/health_test.sh
+++ scripts/health_test.sh
@@ -13,7 +13,7 @@ test_is_version_compatible_invalid_major_version() {
   local min_major="2"
   local min_minor="1"
   assertFalse "required: >= ${min_major}.${min_minor}, actual: ${major}.${minor}"\
-    "is_version_compatible ${min_major} ${min_major} ${major} ${minor}"
+    "is_version_compatible ${min_major} ${min_minor} ${major} ${minor}"
 }
 
 test_is_version_compatible_invalid_minor_version() {
