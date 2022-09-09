ac365240a2c79ca9efcf87b42dd5200c093a8c12
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2022-09-09 11:42:15 -0300

    added test_populate_data.py

--- exasol_integration_test_docker_environment/lib/test_environment/database_setup/populate_data.py
+++ exasol_integration_test_docker_environment/lib/test_environment/database_setup/populate_data.py
@@ -1,15 +1,16 @@
 import logging
-from pathlib import Path
+from pathlib import Path, PurePath
 
 import luigi
 
+from exasol_integration_test_docker_environment.abstract_method_exception import AbstractMethodException
 from exasol_integration_test_docker_environment.lib.base.docker_base_task import DockerBaseTask
 from exasol_integration_test_docker_environment.lib.base.json_pickle_parameter import JsonPickleParameter
 from exasol_integration_test_docker_environment.lib.data.database_credentials import DatabaseCredentialsParameter
 from exasol_integration_test_docker_environment.lib.data.environment_info import EnvironmentInfo
 
 
-class PopulateEngineSmallTestDataToDatabase(DockerBaseTask, DatabaseCredentialsParameter):
+class PopulateTestDataToDatabase(DockerBaseTask, DatabaseCredentialsParameter):
     logger = logging.getLogger('luigi-interface')
 
     environment_name = luigi.Parameter()
@@ -33,9 +34,15 @@ class PopulateEngineSmallTestDataToDatabase(DockerBaseTask, DatabaseCredentialsP
         self.logger.warning("Uploading data")
         username = self.db_user
         password = self.db_password
+        data_path_within_test_container = self.get_data_path_within_test_container()
+        data_file_within_data_path = self.get_data_file_within_data_path()
         with self._get_docker_client() as docker_client:
             test_container = docker_client.containers.get(self._test_container_info.container_name)
-            cmd = f"""cd /tests/test/enginedb_small; $EXAPLUS -c '{self._database_info.host}:{self._database_info.db_port}' -u '{username}' -p '{password}' -f import.sql -jdbcparam 'validateservercertificate=0'"""
+            cmd = f"cd {data_path_within_test_container}; " \
+                  f"$EXAPLUS -c '{self._database_info.host}:{self._database_info.db_port}' " \
+                  f"-u '{username}' -p '{password}' -f {data_file_within_data_path} " \
+                  f"-jdbcparam 'validateservercertificate=0'"
+
             bash_cmd = f"""bash -c "{cmd}" """
             exit_code, output = test_container.exec_run(cmd=bash_cmd)
         self.write_logs(output.decode("utf-8"))
@@ -46,3 +53,9 @@ class PopulateEngineSmallTestDataToDatabase(DockerBaseTask, DatabaseCredentialsP
         log_file = Path(self.get_log_path(), "log")
         with log_file.open("w") as file:
             file.write(output)
+
+    def get_data_path_within_test_container(self) -> PurePath:
+        raise AbstractMethodException()
+
+    def get_data_file_within_data_path(self) -> PurePath:
+        raise AbstractMethodException()
