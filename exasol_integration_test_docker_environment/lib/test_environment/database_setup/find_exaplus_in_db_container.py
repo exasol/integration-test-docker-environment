ea30d9079928e4d98356db6ab42f2c583d104f00
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2022-09-13 09:04:19 -0300

    Updated find command to identify exaplus

--- exasol_integration_test_docker_environment/lib/test_environment/database_setup/find_exaplus_in_db_container.py
+++ exasol_integration_test_docker_environment/lib/test_environment/database_setup/find_exaplus_in_db_container.py
@@ -8,7 +8,7 @@ def find_exaplus(db_container: docker.models.containers.Container) -> PurePath:
     Tries to find path of exaplus in given container in directories where exaplus is normally installed.
     :db_container Container where to search for exaplus
     """
-    exit, output = db_container.exec_run(cmd="find /  \( -name 'exaplus' -and -type f -and -path '/usr/opt/*' -or -path '/opt/exasol/*' \)")
+    exit, output = db_container.exec_run(cmd="find /usr/opt /opt  -name 'exaplus' -type f")
     if exit != 0:
         raise RuntimeError(f"Exaplus not found on docker db! Output is {output}")
     found_paths = list(filter(None, output.decode("UTF-8").split("\n")))
