ae16f6b327f436eb51c945c7abb5915ca482de5d
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2022-09-23 15:40:19 -0300

    suppress 'myst.reference' warning

--- doc/conf.py
+++ doc/conf.py
@@ -55,6 +55,8 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '.build-docu']
 
+suppress_warnings = ['myst.header']
+
 
 # -- Options for HTML output -------------------------------------------------
 
