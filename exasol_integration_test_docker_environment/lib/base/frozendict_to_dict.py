31674550877813545a8f3886331726c7d5c13106
Nicola Coretti <nicola.coretti@exasol.com>
2023-02-13 11:09:05 +0100

    Add support for itde command line tool installation
    Co-authored-by: Nicola Coretti <nicola.coretti@gmail.com>

--- exasol_integration_test_docker_environment/lib/base/frozendict_to_dict.py
+++ exasol_integration_test_docker_environment/lib/base/frozendict_to_dict.py
@@ -1,10 +1,11 @@
-from collections import Mapping
+from collections.abc import Mapping
+
 
 class FrozenDictToDict():
     def convert(self, obj):
         if isinstance(obj, Mapping):
             return {k: self.convert(v) for k, v in obj.items()}
-        elif isinstance(obj, (list,tuple)):
+        elif isinstance(obj, (list, tuple)):
             return [self.convert(o) for o in obj]
         else:
             return obj
