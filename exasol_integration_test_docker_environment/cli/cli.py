53f396c527740ef7a3965f03807f74eb1ba010f8
Nicola Coretti <nicola.coretti@gmail.com>
2023-03-20 10:38:31 +0100

    Fix itde command and subcommands

--- exasol_integration_test_docker_environment/cli/cli.py
+++ exasol_integration_test_docker_environment/cli/cli.py
@@ -3,4 +3,22 @@ import click
 
 @click.group()
 def cli():
+    """
+    ITDE - Itegration Test Docker Environemnt
+
+    Create and control a docker based exasol database test setup.
+
+    Examples:
+
+        Check the health of the execution environment:
+
+            $ itde health
+
+        Spwan a itde test environment:
+
+            $ itde spawn-test-environment --environment-name test \\
+            --database-port-forward 8888 --bucketfs-port-forward 6666 \\
+            --docker-db-image-version 7.1.9 --db-mem-size 4GB
+
+    """
     pass
