243ff21bbb4577a4de983bdf049d83fffa9c69d8
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2023-04-13 12:55:55 -0300

    Revert "updated version"
    This reverts commit c87510fd60f6019289e8a4d283ba00e763eb21a3.

--- setup.py
+++ setup.py
@@ -103,7 +103,7 @@ entry_points = \
 
 setup_kwargs = {
     'name': 'exasol-integration-test-docker-environment',
-    'version': '1.6.0',
+    'version': '1.5.0',
     'description': 'Integration Test Docker Environment for Exasol',
     'long_description': 'Integration Test Docker Environment\n===================================\n\nThis project provides a command line interface and a Python API layer to\nstart a test environment with an `Exasol\nDocker-DB <https://hub.docker.com/r/exasol/docker-db>`_. Both start an\nExasol Docker-DB container, but the API Layer has extended functionality\nand also can start an associated test container for whose content the\nclient is responsible.\n\nDocumentation\n-------------\n\n`Documentation for the current main branch <https://exasol.github.io/integration-test-docker-environment/main>`_ is hosted on the Github Pages of this project.\n`Here <https://exasol.github.io/integration-test-docker-environment>`_  is a list of documentations for previous releases.\n',
     'author': 'Torsten Kilias',
