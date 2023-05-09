b4972dcb0add70ce7db9967f9a56d171a14ba836
Torsten Kilias <tkilias@users.noreply.github.com>
2023-05-09 17:54:43 +0200

    #311: Add vagrant setup (#312)

---
+++ vagrant/provisioning/dns.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -euo pipefail
+
+sudo sed -i 's/DNS=.*/DNS=8.8.8.8/g' /etc/systemd/resolved.conf
+sudo service systemd-resolved restart
+sudo rm -f /etc/resolv.conf
+sudo ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf
+
