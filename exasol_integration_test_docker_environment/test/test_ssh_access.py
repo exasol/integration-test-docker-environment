7b7ebd57638c25b4ae106f82d4e1def3bdbe5e03
KK <christoph.kuhnke@exasol.com>
2023-05-17 10:36:25 +0200

    added test logic

--- exasol_integration_test_docker_environment/test/test_ssh_access.py
+++ exasol_integration_test_docker_environment/test/test_ssh_access.py
@@ -5,6 +5,7 @@ import unittest
 from exasol_integration_test_docker_environment.testing import utils
 from exasol_integration_test_docker_environment.testing \
    .exaslct_test_environment import ExaslctTestEnvironment
+from exasol_integration_test_docker_environment.lib.base.ssh_access import SshKey
 
 
 class DockerTestEnvironmentTest(unittest.TestCase):
@@ -26,7 +27,6 @@ class DockerTestEnvironmentTest(unittest.TestCase):
     def setUpClass(cls):
         print(f"SetUp {cls.__name__}")
         cls.environment = ExaslctTestEnvironment(cls, "itde", clean_images_at_close=False)
-        # TODO cls.environment.clean_images()
         cls.docker_environment_name = cls.__name__
         cls.spawned = cls.environment.spawn_docker_test_environments(
             name=cls.docker_environment_name,
@@ -36,6 +36,10 @@ class DockerTestEnvironmentTest(unittest.TestCase):
     @classmethod
     def tearDownClass(cls):
         utils.close_environments(cls.spawned, cls.environment)
+
+    def test_generate_ssh_key_file(self):
+        file = SshKey.default_folder() / "id_rsa"
+        self.assertTrue(file.exists())
 
 
 if __name__ == '__main__':
