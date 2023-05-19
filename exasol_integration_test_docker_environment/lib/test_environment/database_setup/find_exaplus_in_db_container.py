306a4f8b4887c75350105b5601a6cafca577c057
Torsten Kilias <torsten.kilias@exasol.com>
2023-05-19 16:35:29 +0200

    Add support for exasol/docker-db:prerelease-8.17.0
    - Extend db_version.py to allow prereleases
    - Fix find_exaplus in case /usr/opt doesn't exist in the docker-db
      container
    - Readd update_packaging.sh to the precommit hook, which was accidently
      removed in #317

--- exasol_integration_test_docker_environment/lib/test_environment/database_setup/find_exaplus_in_db_container.py
+++ exasol_integration_test_docker_environment/lib/test_environment/database_setup/find_exaplus_in_db_container.py
@@ -8,7 +8,11 @@ def find_exaplus(db_container: docker.models.containers.Container) -> PurePath:
     Tries to find path of exaplus in given container in directories where exaplus is normally installed.
     :db_container Container where to search for exaplus
     """
-    exit, output = db_container.exec_run(cmd="find /usr/opt /opt  -name 'exaplus' -type f")
+    exit, output = db_container.exec_run(cmd="find /usr/opt -name 'exaplus' -type f")
+    if exit != 0:
+        # Using /usr/opt and /opt together in one command doesn't work, because in Exasol 8, /usr/opt doesn't exist
+        # and as such the command fails, even if it finds exaplus in /opt
+        exit, output = db_container.exec_run(cmd="find /opt -name 'exaplus' -type f")
     if exit != 0:
         raise RuntimeError(f"Exaplus not found on docker db! Output is {output}")
     found_paths = list(filter(None, output.decode("UTF-8").split("\n")))
