250bf2be4e4f2951cf79a8e30108ce757f91af5a
KK <christoph.kuhnke@exasol.com>
2023-05-19 13:27:05 +0200

    used logger

--- exasol_integration_test_docker_environment/test/conftest.py
+++ exasol_integration_test_docker_environment/test/conftest.py
@@ -1,14 +1,19 @@
+import logging
 import pytest
 
 from exasol_integration_test_docker_environment.testing import utils
 from exasol_integration_test_docker_environment.testing \
    .exaslct_test_environment import ExaslctTestEnvironment
 
+
+_logger = logging.getLogger(__name__)
+
+
 class PyTestEnvironment:
     def __init__(self):
-        # print(f"SetUp {cls.__name__}")
-        self.environment = ExaslctTestEnvironment(self, "itde", clean_images_at_close=False)
         self.docker_environment_name = self.__class__.__name__
+        _logger.info(f"SetUp {self.docker_environment_name}")
+        self.environment = ExaslctTestEnvironment(self, "itde", clean_images_at_close=False)
         self.spawned = self.environment.spawn_docker_test_environments(
             name=self.docker_environment_name,
             additional_parameter = ["--docker-access-method", "SSH"],
