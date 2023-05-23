3df451653ea391a0b0bbfa3e96c2ba85b9359b86
KK <christoph.kuhnke@exasol.com>
2023-05-23 13:13:29 +0200

    moved contextmanager data base to conftest.py

--- exasol_integration_test_docker_environment/test/conftest.py
+++ exasol_integration_test_docker_environment/test/conftest.py
@@ -1,12 +1,14 @@
+import contextlib
 import logging
 import pytest
 from typing import Iterator, List, Optional
 
 from exasol_integration_test_docker_environment.testing import utils
 from exasol_integration_test_docker_environment.testing \
-   .exaslct_test_environment import ExaslctTestEnvironment
-from exasol_integration_test_docker_environment.testing \
-   .exaslct_test_environment import SpawnedTestEnvironments
+   .exaslct_test_environment import (
+       ExaslctTestEnvironment,
+       SpawnedTestEnvironments,
+   )
 
 
 @pytest.fixture
@@ -22,3 +24,19 @@ def itde_cli_test_isolation(request) -> Iterator[ExaslctTestEnvironment]:
         yield environment
     finally:
         utils.close_environments(environment)
+
+
+@contextlib.contextmanager
+def database(itde_test_isolation: ExaslctTestEnvironment,
+             name: Optional[str] = None,
+             additional_parameters: Optional[List[str]] = None
+             ) -> Iterator[SpawnedTestEnvironments]:
+    name = name if name else itde_test_isolation.name
+    spawned = itde_test_isolation.spawn_docker_test_environments(
+        name=name,
+        additional_parameter=additional_parameters,
+    )
+    try:
+        yield spawned
+    finally:
+        utils.close_environments(spawned)
--- exasol_integration_test_docker_environment/test/test_ssh_access.py
+++ exasol_integration_test_docker_environment/test/test_ssh_access.py
@@ -1,30 +1,8 @@
-import contextlib
+# import contextlib
 import pytest
 
-from typing import Iterator, List, Optional
-
-from exasol_integration_test_docker_environment.testing import utils
-from exasol_integration_test_docker_environment.testing \
-   .exaslct_test_environment import ExaslctTestEnvironment
-from exasol_integration_test_docker_environment.testing \
-   .exaslct_test_environment import SpawnedTestEnvironments
 from exasol_integration_test_docker_environment.lib.base.ssh_access import SshKey
-
-
-@contextlib.contextmanager
-def database(itde_test_isolation: ExaslctTestEnvironment,
-             name: Optional[str] = None,
-             additional_parameters: Optional[List[str]] = None
-             ) -> Iterator[SpawnedTestEnvironments]:
-    name = name if name else itde_test_isolation.name
-    spawned = itde_test_isolation.spawn_docker_test_environments(
-        name=name,
-        additional_parameter=additional_parameters,
-    )
-    try:
-        yield spawned
-    finally:
-        utils.close_environments(spawned)
+from exasol_integration_test_docker_environment.test.conftest import database
 
 
 def test_generate_ssh_key_file(itde_cli_test_isolation):
