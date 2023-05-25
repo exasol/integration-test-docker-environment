22dd095949da64930de237864394f8f0f4be6c97
KK <christoph.kuhnke@exasol.com>
2023-05-25 12:51:39 +0200

    Fixed name of import

--- test/integration/test_ssh_access.py
+++ test/integration/test_ssh_access.py
@@ -2,7 +2,7 @@ import os
 import pytest
 
 from conftest import find_container
-from exasol_integration_test_docker_environment.lib.base.ssh_access import SshCache
+from exasol_integration_test_docker_environment.lib.base.ssh_access import SshKeyCache
 
 
 def test_generate_ssh_key_file(api_database):
