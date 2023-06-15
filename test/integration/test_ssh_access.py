206b71bb087e3007a1a586bf95647eeee9c91ea3
Torsten Kilias <torsten.kilias@exasol.com>
2023-06-15 08:57:42 +0200

    Fix test_ssh_access.py which was broken from the beginning

--- test/integration/test_ssh_access.py
+++ test/integration/test_ssh_access.py
@@ -1,16 +1,13 @@
-import os
-import pytest
-
-from conftest import container_named
 from exasol_integration_test_docker_environment.lib.base.ssh_access import SshKeyCache
+from test.integration.helpers import container_named
 
 
 def test_generate_ssh_key_file(api_database):
-    params = { "db_os_access": "SSH" }
+    params = {"db_os_access": "SSH"}
     with api_database(additional_parameters=params) as db:
         cache = SshKeyCache()
         container_name = db.environment_info.database_info.container_info.container_name
         with container_named(container_name) as container:
             command = container.exec_run("cat /root/.ssh/authorized_keys")
-    assert cache.private_key.exists()
-    " itde-ssh-access" in command[1].decode("utf-8")
+    assert cache.private_key.exists() and \
+           " itde-ssh-access" in command[1].decode("utf-8")
