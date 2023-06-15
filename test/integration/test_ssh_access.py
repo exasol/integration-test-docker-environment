b0eb613ac6c1b4f25b8c15cf76675dd6ec276336
Torsten Kilias <torsten.kilias@exasol.com>
2023-06-15 09:54:26 +0200

    Try to fix test_ssh_access.py again

--- test/integration/test_ssh_access.py
+++ test/integration/test_ssh_access.py
@@ -3,11 +3,11 @@ from test.integration.helpers import container_named
 
 
 def test_generate_ssh_key_file(api_database):
-    params = {"db_os_access": "SSH"}
+    params = { "db_os_access": "SSH" }
     with api_database(additional_parameters=params) as db:
         cache = SshKeyCache()
         container_name = db.environment_info.database_info.container_info.container_name
         with container_named(container_name) as container:
             command = container.exec_run("cat /root/.ssh/authorized_keys")
-    assert cache.private_key.exists() and \
-           " itde-ssh-access" in command[1].decode("utf-8")
+    assert cache.private_key.exists()
+    " itde-ssh-access" in command[1].decode("utf-8")
