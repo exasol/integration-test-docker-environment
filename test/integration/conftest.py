a12d5ebd76587cd8e3688cc90b9c061de8b852d7
KK <christoph.kuhnke@exasol.com>
2023-06-15 17:23:44 +0200

    Moved container_named to test_ssh_access.py
    to avoid need to import conftest.py

--- test/integration/conftest.py
+++ test/integration/conftest.py
@@ -101,19 +101,3 @@ def api_database(api_isolation: ApiTestEnvironment) -> ApiContextProvider:
         utils.close_environments(spawned)
 
     return create_context
-
-
-def exact_matcher(names):
-    return lambda value: all(x == value for x in names)
-
-
-def superset_matcher(names):
-    return lambda value: all(x in value for x in names)
-
-
-@contextlib.contextmanager
-def container_named(*names, matcher=None):
-    matcher = matcher if matcher else exact_matcher(names)
-    with ContextDockerClient() as client:
-        matches = [c for c in client.containers.list() if matcher(c.name)]
-        yield matches[0] if matches else None
--- test/integration/test_ssh_access.py
+++ test/integration/test_ssh_access.py
@@ -1,10 +1,27 @@
+import contextlib
 import fabric
 import io
 import os
 import pytest
 
-from .conftest import container_named
+from exasol_integration_test_docker_environment.lib.docker import ContextDockerClient
 from exasol_integration_test_docker_environment.lib.base.ssh_access import SshKey, SshKeyCache
+
+
+def exact_matcher(names):
+    return lambda value: all(x == value for x in names)
+
+
+def superset_matcher(names):
+    return lambda value: all(x in value for x in names)
+
+
+@contextlib.contextmanager
+def container_named(*names, matcher=None):
+    matcher = matcher if matcher else exact_matcher(names)
+    with ContextDockerClient() as client:
+        matches = [c for c in client.containers.list() if matcher(c.name)]
+        yield matches[0] if matches else None
 
 
 def test_generate_ssh_key_file(api_database):
