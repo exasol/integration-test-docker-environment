969cdcc3848af84f56fd4cb7a74d13c5103a6057
Torsten Kilias <torsten.kilias@exasol.com>
2023-06-16 15:32:40 +0200

    Add workaround for not working UDFs to EXAConf of 8.18.1 and simplified the test UDF

--- test/integration/test_udf_execution.py
+++ test/integration/test_udf_execution.py
@@ -3,32 +3,36 @@ import pyexasol
 from inspect import cleandoc
 from time import sleep
 
+from exasol_integration_test_docker_environment.lib.test_environment.db_version import DbVersion
+
+
 def test_udf_execution(api_database):
     def wait_until_container_is_unpacked():
-        sleep(5*60)
+        sleep(5 * 60)
 
     def udf_sql(schema: str) -> str:
         return cleandoc(
             f"""
             --/
             CREATE OR REPLACE PYTHON3 SCALAR SCRIPT
-              {schema}.bucketfs_ls(my_path VARCHAR(256))
-              EMITS (files VARCHAR(256)) AS
+              {schema}.python3_test_udf(count INT)
+              EMITS (outp INT) AS
             import os
 
             def run(ctx):
-                    for line in os.listdir(ctx.my_path):
-                            ctx.emit(line)
+                for i in range(ctx.count):
+                    ctx.emit(i)
             /
             """
         )
+
     with api_database() as db:
         dbinfo = db.environment_info.database_info
         dsn = f"{dbinfo.host}:{dbinfo.db_port}"
         connection = pyexasol.connect(dsn=dsn, user="sys", password="exasol")
-        wait_until_container_is_unpacked()
+        if DbVersion.from_db_version_str(db.docker_db_image_version).major == 7:
+            wait_until_container_is_unpacked()
         connection.execute("CREATE SCHEMA IF NOT EXISTS S")
         connection.execute(udf_sql(schema="S"))
-        result = connection.execute("SELECT S.bucketfs_ls('/buckets/bfsdefault/default')")
-        result = next(result)[0]
-        assert result == "EXAClusterOS"
+        result = connection.execute("SELECT S.python3_test_udf(10)").fetchall()
+        assert result == [(i,) for i in range(10)]
