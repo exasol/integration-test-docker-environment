41c8c95b44fbbeab8f71053f5cf2de88014d2f01
KK <christoph.kuhnke@exasol.com>
2023-06-16 08:39:30 +0200

    fixed initial sample call

--- test/integration/test_udf_execution.py
+++ test/integration/test_udf_execution.py
@@ -4,7 +4,8 @@ import pyexasol
 def test_udf_execution(api_database):
     with api_database() as db:
         dbinfo = db.environment_info.database_info
-        dsn = "{dbinfo.host}:{dbinfo.db_port}"
+        dsn = f"{dbinfo.host}:{dbinfo.db_port}"
         connection = pyexasol.connect(dsn=dsn, user="sys", password="exasol")
         result = connection.execute("select 1 from dual")
-        assert result[0] == 1
+        result = next(result)[0]
+        assert result == 1
