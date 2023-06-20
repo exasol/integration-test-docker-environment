9de3beaa951dea21fff1197c0a6fbb4b6321e48d
KK <christoph.kuhnke@exasol.com>
2023-06-20 09:20:40 +0200

    fixed review finding

--- test/integration/test_ssh_access.py
+++ test/integration/test_ssh_access.py
@@ -6,22 +6,7 @@ import pytest
 
 from exasol_integration_test_docker_environment.lib.docker import ContextDockerClient
 from exasol_integration_test_docker_environment.lib.base.ssh_access import SshKey, SshKeyCache
-
-
-def exact_matcher(names):
-    return lambda value: all(x == value for x in names)
-
-
-def superset_matcher(names):
-    return lambda value: all(x in value for x in names)
-
-
-@contextlib.contextmanager
-def container_named(*names, matcher=None):
-    matcher = matcher if matcher else exact_matcher(names)
-    with ContextDockerClient() as client:
-        matches = [c for c in client.containers.list() if matcher(c.name)]
-        yield matches[0] if matches else None
+from .helpers import container_named
 
 
 def test_generate_ssh_key_file(api_database):
