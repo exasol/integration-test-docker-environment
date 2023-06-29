9bfd3af9de7db39aeaa15c880a398ec6b4667ec4
KK <christoph.kuhnke@exasol.com>
2023-06-29 15:05:10 +0200

    changed noxfile

--- noxfile.py
+++ noxfile.py
@@ -172,8 +172,8 @@ def get_db_versions() -> List[str]:
 @nox.parametrize("db_version", get_db_versions())
 def run_tests(session: nox.Session, db_version: str):
     """Run the tests in the poetry environment"""
+    env = {"EXASOL_VERSION": db_version}
     with session.chdir(ROOT):
-        env = {"EXASOL_VERSION": db_version}
         session.run(
             "python",
             "-u",
@@ -187,7 +187,7 @@ def run_tests(session: nox.Session, db_version: str):
     session.run(
         "pytest", '--log-level=DEBUG',
         "-s",
-        "--itde-db-version", db_version, './test/integration/pytest_itde_test.py'
+        './test/integration/pytest_itde_test.py'
     )
 
 
