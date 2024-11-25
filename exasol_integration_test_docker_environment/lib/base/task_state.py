f039076debb2c7bd353a33cd1788c7bf17d271f5
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2024-11-25 10:01:57 -0300

    Fixed task state

--- exasol_integration_test_docker_environment/lib/base/task_state.py
+++ exasol_integration_test_docker_environment/lib/base/task_state.py
@@ -2,11 +2,11 @@ from enum import Enum
 
 
 class TaskState(Enum):
-    NONE = (0,)
-    INIT = (1,)
-    AFTER_INIT = (2,)
-    RUN = (3,)
-    FINISHED = (4,)
-    CLEANUP = (5,)
-    CLEANED = (6,)
+    NONE = 0
+    INIT = 1
+    AFTER_INIT = 2
+    RUN = 3
+    FINISHED = 4
+    CLEANUP = 5
+    CLEANED = 6
     ERROR = 7
