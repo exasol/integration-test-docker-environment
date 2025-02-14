ed02f3ab84c7bc2984f8193152ac0f1f189222e2
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2025-02-14 13:02:31 -0300

    Fixed flavor_task.py and formatting

--- exasol_integration_test_docker_environment/lib/base/flavor_task.py
+++ exasol_integration_test_docker_environment/lib/base/flavor_task.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from typing import (
     Any,
     Dict,
+    Tuple,
     Type,
 )
 
--- exasol_integration_test_docker_environment/lib/test_environment/parameter/general_spawn_test_environment_parameter.py
+++ exasol_integration_test_docker_environment/lib/test_environment/parameter/general_spawn_test_environment_parameter.py
@@ -1,6 +1,6 @@
 from typing import (
-    Tuple,
     Optional,
+    Tuple,
 )
 
 import luigi
