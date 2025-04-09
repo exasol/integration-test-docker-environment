3bf56b6f8bfef06d76f80425bc33de8f9e78fbeb
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2025-04-09 14:04:05 -0300

    Fixed formatting in test_api_logging.py

--- test/integration/test_api_logging.py
+++ test/integration/test_api_logging.py
@@ -72,13 +72,13 @@ class DummyTask(DependencyLoggerBaseTask):
         self.return_object("DUMMY SUCCESS")
 
 
-
 def configure_logging(log_level: int):
     logging.basicConfig(
         format=f"{TEST_FORMAT} %(levelname)s %(message)s",
         level=log_level,
         force=True,
     )
+
 
 def reset_logging():
     logging.basicConfig(
@@ -100,7 +100,9 @@ def custom_logging():
         configure_logging(log_level)
         yield
         reset_logging()
+
     return logging_context_creator
+
 
 @pytest.fixture
 def luigi_output(tmp_path):
@@ -118,9 +120,7 @@ def luigi_output(tmp_path):
 
 
 @ignore_resource_warning()
-def dummy_api_command(
-    log_level: Optional[str], use_job_specific_log_file: bool
-):
+def dummy_api_command(log_level: Optional[str], use_job_specific_log_file: bool):
     task_creator = lambda: generate_root_task(task_class=DummyTask)
     result = run_task(
         task_creator=task_creator,
@@ -134,16 +134,26 @@ def dummy_api_command(
 
 def assert_loggers_are_equal(logger_infos_after, logger_infos_before):
     assert logger_infos_before[ROOT_LOGGER] == logger_infos_after[ROOT_LOGGER]
-    assert logger_infos_before[API_CLIENT_LOGGING_TEST_LOGGER] == logger_infos_after[API_CLIENT_LOGGING_TEST_LOGGER]
+    assert (
+        logger_infos_before[API_CLIENT_LOGGING_TEST_LOGGER]
+        == logger_infos_after[API_CLIENT_LOGGING_TEST_LOGGER]
+    )
     assert logger_infos_before[LUIGI_LOGGER] == logger_infos_after[LUIGI_LOGGER]
-    assert logger_infos_before[LUIGI_INTERFACE_LOGGER] == logger_infos_after[LUIGI_INTERFACE_LOGGER]
+    assert (
+        logger_infos_before[LUIGI_INTERFACE_LOGGER]
+        == logger_infos_after[LUIGI_INTERFACE_LOGGER]
+    )
+
 
 def create_test_regex(log_level: int):
     level_name = logging.getLevelName(log_level)
     regex = f".*{TEST_FORMAT} {level_name} DummyTask_.* DUMMY LOGGER {level_name}.*"
     return re.compile(regex)
 
-def test_luigi_log_level_info_and_basic_logging_error(capfd, custom_logging, luigi_output):
+
+def test_luigi_log_level_info_and_basic_logging_error(
+    capfd, custom_logging, luigi_output
+):
     """
     This test checks if setting luigi log level to level INFO and global log level to ERROR
     prints error/info messages to stderr.
@@ -152,9 +162,7 @@ def test_luigi_log_level_info_and_basic_logging_error(capfd, custom_logging, lui
 
     with custom_logging(log_level=logging.ERROR) as error_log:
         logger_infos_before = create_logger_infos()
-        result = dummy_api_command(
-            log_level="INFO", use_job_specific_log_file=False
-        )
+        result = dummy_api_command(log_level="INFO", use_job_specific_log_file=False)
         logger_infos_after = create_logger_infos()
         assert_loggers_are_equal(logger_infos_after, logger_infos_before)
 
@@ -163,15 +171,18 @@ def test_luigi_log_level_info_and_basic_logging_error(capfd, custom_logging, lui
         assert stderr_output != ""
         assert create_test_regex(logging.ERROR).search(stderr_output) is not None
         assert create_test_regex(logging.INFO).search(stderr_output) is not None
-        assert re.compile(".*===== Luigi Execution Summary =====.*").search(stderr_output) is not None
-
-        main_log_glob = list(
-            luigi_output.glob("**/main.log")
+        assert (
+            re.compile(".*===== Luigi Execution Summary =====.*").search(stderr_output)
+            is not None
         )
+
+        main_log_glob = list(luigi_output.glob("**/main.log"))
         assert main_log_glob == []
 
 
-def test_luigi_log_level_error_and_basic_logging_info(capfd, custom_logging, luigi_output):
+def test_luigi_log_level_error_and_basic_logging_info(
+    capfd, custom_logging, luigi_output
+):
     """
     This test checks if setting luigi log level to level ERROR and global log level to INFO
     prints only error messages to stderr, but no INFO messages to stderr.
@@ -179,9 +190,7 @@ def test_luigi_log_level_error_and_basic_logging_info(capfd, custom_logging, lui
     """
     with custom_logging(log_level=logging.INFO) as error_log:
         logger_infos_before = create_logger_infos()
-        result = dummy_api_command(
-            log_level="ERROR", use_job_specific_log_file=False
-        )
+        result = dummy_api_command(log_level="ERROR", use_job_specific_log_file=False)
         logger_infos_after = create_logger_infos()
         assert_loggers_are_equal(logger_infos_after, logger_infos_before)
         stdout_output, stderr_output = capfd.readouterr()
@@ -189,12 +198,13 @@ def test_luigi_log_level_error_and_basic_logging_info(capfd, custom_logging, lui
         assert stderr_output != ""
         assert create_test_regex(logging.ERROR).search(stderr_output) is not None
         assert create_test_regex(logging.INFO).search(stderr_output) is None
-        main_log_glob = list(
-            luigi_output.glob("**/main.log")
-        )
+        main_log_glob = list(luigi_output.glob("**/main.log"))
         assert main_log_glob == []
 
-def test_luigi_log_level_error_multiple_calls_and_basic_logging_info(capfd, custom_logging, luigi_output):
+
+def test_luigi_log_level_error_multiple_calls_and_basic_logging_info(
+    capfd, custom_logging, luigi_output
+):
     """
     This test checks if setting luigi log level to level ERROR and global log level to INFO
     prints only error messages to stderr, but no INFO messages; even when running a task multiple times.
@@ -203,12 +213,8 @@ def test_luigi_log_level_error_multiple_calls_and_basic_logging_info(capfd, cust
     with custom_logging(log_level=logging.INFO) as error_log:
 
         logger_infos_before = create_logger_infos()
-        result = dummy_api_command(
-            log_level="ERROR", use_job_specific_log_file=False
-        )
-        result = dummy_api_command(
-            log_level="ERROR", use_job_specific_log_file=False
-        )
+        result = dummy_api_command(log_level="ERROR", use_job_specific_log_file=False)
+        result = dummy_api_command(log_level="ERROR", use_job_specific_log_file=False)
         logger_infos_after = create_logger_infos()
         assert_loggers_are_equal(logger_infos_after, logger_infos_before)
         stdout_output, stderr_output = capfd.readouterr()
@@ -217,12 +223,13 @@ def test_luigi_log_level_error_multiple_calls_and_basic_logging_info(capfd, cust
         assert create_test_regex(logging.ERROR).search(stderr_output) is not None
         assert create_test_regex(logging.INFO).search(stderr_output) is None
         assert 2 == stderr_output.count("DUMMY LOGGER ERROR")
-        main_log_glob = list(
-            luigi_output.glob("**/main.log")
-        )
+        main_log_glob = list(luigi_output.glob("**/main.log"))
         assert main_log_glob == []
 
-def test_luigi_use_job_specific_log_file_and_basic_logging_error(capfd, custom_logging, luigi_output):
+
+def test_luigi_use_job_specific_log_file_and_basic_logging_error(
+    capfd, custom_logging, luigi_output
+):
     """
     This test checks if setting luigi log level to None and global log level to ERROR, and using flag
     `use_job_specific_log_file`
@@ -233,9 +240,7 @@ def test_luigi_use_job_specific_log_file_and_basic_logging_error(capfd, custom_l
     with custom_logging(log_level=logging.ERROR) as error_log:
 
         logger_infos_before = create_logger_infos()
-        result = dummy_api_command(
-            log_level=None, use_job_specific_log_file=True
-        )
+        result = dummy_api_command(log_level=None, use_job_specific_log_file=True)
         logger_infos_after = create_logger_infos()
         assert_loggers_are_equal(logger_infos_after, logger_infos_before)
         stdout_output, stderr_output = capfd.readouterr()
@@ -246,15 +251,19 @@ def test_luigi_use_job_specific_log_file_and_basic_logging_error(capfd, custom_l
         else:
             stderr_output_lines = stderr_output.split("\n")
             assert len(stderr_output_lines) == 3
-            assert " is multi-threaded, use of fork() may lead to deadlocks in the child" in stderr_output_lines[0]
+            assert (
+                " is multi-threaded, use of fork() may lead to deadlocks in the child"
+                in stderr_output_lines[0]
+            )
             assert stderr_output_lines[1] == "  self.pid = os.fork()"
             assert stderr_output_lines[2] == ""
-        main_log_glob = list(
-            luigi_output.glob("**/main.log")
-        )
+        main_log_glob = list(luigi_output.glob("**/main.log"))
         assert len(main_log_glob) > 0
 
-def test_luigi_no_log_config_and_basic_logging_info(capfd, custom_logging, luigi_output):
+
+def test_luigi_no_log_config_and_basic_logging_info(
+    capfd, custom_logging, luigi_output
+):
     """
     This test checks if setting luigi log level to None and global log level to INFO
     prints error/info messages to stderr.
@@ -262,9 +271,7 @@ def test_luigi_no_log_config_and_basic_logging_info(capfd, custom_logging, luigi
     """
     with custom_logging(log_level=logging.INFO) as error_log:
         logger_infos_before = create_logger_infos()
-        result = dummy_api_command(
-            log_level=None, use_job_specific_log_file=False
-        )
+        result = dummy_api_command(log_level=None, use_job_specific_log_file=False)
         logger_infos_after = create_logger_infos()
         assert_loggers_are_equal(logger_infos_after, logger_infos_before)
         stdout_output, stderr_output = capfd.readouterr()
@@ -273,13 +280,17 @@ def test_luigi_no_log_config_and_basic_logging_info(capfd, custom_logging, luigi
 
         assert create_test_regex(logging.ERROR).search(stderr_output) is not None
         assert create_test_regex(logging.INFO).search(stderr_output) is not None
-        assert re.compile(".*===== Luigi Execution Summary =====.*").search(stderr_output) is not None
-        main_log_glob = list(
-            luigi_output.glob("**/main.log")
+        assert (
+            re.compile(".*===== Luigi Execution Summary =====.*").search(stderr_output)
+            is not None
         )
+        main_log_glob = list(luigi_output.glob("**/main.log"))
         assert main_log_glob == []
 
-def test_luigi_no_log_config_and_basic_logging_error(capfd, custom_logging, luigi_output):
+
+def test_luigi_no_log_config_and_basic_logging_error(
+    capfd, custom_logging, luigi_output
+):
     """
     This test checks if setting luigi log level to None and global log level to ERROR
     prints error messages to stderr, but no INFO messages.
@@ -287,9 +298,7 @@ def test_luigi_no_log_config_and_basic_logging_error(capfd, custom_logging, luig
     """
     with custom_logging(log_level=logging.ERROR) as error_log:
         logger_infos_before = create_logger_infos()
-        result = dummy_api_command(
-            log_level=None, use_job_specific_log_file=False
-        )
+        result = dummy_api_command(log_level=None, use_job_specific_log_file=False)
         logger_infos_after = create_logger_infos()
         assert_loggers_are_equal(logger_infos_after, logger_infos_before)
         stdout_output, stderr_output = capfd.readouterr()
@@ -298,10 +307,9 @@ def test_luigi_no_log_config_and_basic_logging_error(capfd, custom_logging, luig
 
         assert create_test_regex(logging.ERROR).search(stderr_output) is not None
         assert create_test_regex(logging.INFO).search(stderr_output) is None
-        main_log_glob = list(
-            luigi_output.glob("**/main.log")
-        )
+        main_log_glob = list(luigi_output.glob("**/main.log"))
         assert main_log_glob == []
+
 
 def create_logger_infos() -> Dict[str, Dict[str, Any]]:
     logger_infos = {
@@ -310,11 +318,10 @@ def create_logger_infos() -> Dict[str, Dict[str, Any]]:
             logging.getLogger("APIClientLoggingTest")
         ),
         LUIGI_LOGGER: get_logger_info(logging.getLogger("luigi")),
-        LUIGI_INTERFACE_LOGGER: get_logger_info(
-            logging.getLogger("luigi-interface")
-        ),
+        LUIGI_INTERFACE_LOGGER: get_logger_info(logging.getLogger("luigi-interface")),
     }
     return logger_infos
+
 
 def get_logger_info(logger: logging.Logger) -> Dict[str, Any]:
     logger_info: Dict[str, Any] = dict()
@@ -328,4 +335,3 @@ def get_logger_info(logger: logging.Logger) -> Dict[str, Any]:
     logger_info[PROPAGATE] = logger.propagate
     logger_info[PARENT] = logger.parent
     return logger_info
-
