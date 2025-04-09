8f6b84c0aff37681f666bff69750214296a81a6b
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2025-04-09 09:45:22 -0300

    Reformatted test_api_build_test_container.py

--- test/integration/test_api_build_test_container.py
+++ test/integration/test_api_build_test_container.py
@@ -11,7 +11,6 @@ from exasol_integration_test_docker_environment.test.get_test_container_content 
 TEST_CONTAINER_CONTENT = get_test_container_content(MOCK_TEST_CONTAINER_PATH)
 
 
-
 def test_build_test_container(api_isolation):
     docker_repository_name = api_isolation.docker_repository_name
     image_info = api.build_test_container(
@@ -24,6 +23,7 @@ def test_build_test_container(api_isolation):
         assert len(image.tags) == 1
         assert image.tags[0] == image_info.get_target_complete_name()
         assert image_info.image_state == ImageState.WAS_BUILD.name
+
 
 def test_build_test_container_use_cached(api_isolation):
     docker_repository_name = api_isolation.docker_repository_name
@@ -39,6 +39,7 @@ def test_build_test_container_use_cached(api_isolation):
         output_directory=api_isolation.output_dir,
     )
     assert image_info2.image_state == ImageState.USED_LOCAL.name
+
 
 def test_build_test_container_force_rebuild(api_isolation):
     docker_repository_name = api_isolation.docker_repository_name
