dc9782fae2106cccd2e90927721e352ded06af69
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2025-04-10 14:15:12 -0300

    Added test_base_task.py to minimal tests

--- noxfile.py
+++ noxfile.py
@@ -78,6 +78,7 @@ def run_minimal_tests(session: nox.Session, db_version: str):
             "test_cli_environment.py",
             "test_db_container_log_thread.py",
             "test_api_logging.py",
+            "test_base_task.py"
         ],
         "unit": ["./test/unit"],
     }
