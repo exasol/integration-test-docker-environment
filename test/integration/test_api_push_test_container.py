5ebf9893a2245de61b964e3ee944339e08e2af98
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2025-04-10 13:33:16 -0300

    Fixed formatting

--- test/integration/test_api_push_test_container.py
+++ test/integration/test_api_push_test_container.py
@@ -10,21 +10,20 @@ from exasol_integration_test_docker_environment.testing.docker_registry import (
 )
 
 
-
 def test_docker_push(api_isolation):
-    with LocalDockerRegistryContextManager(
-        api_isolation.name
-    ) as docker_registry:
+    with LocalDockerRegistryContextManager(api_isolation.name) as docker_registry:
         docker_repository_name = docker_registry.name
         try:
             image_info = api.push_test_container(
                 source_docker_repository_name=docker_repository_name,
                 target_docker_repository_name=docker_repository_name,
                 test_container_content=get_test_container_content(),
-                output_directory=api_isolation.output_dir
+                output_directory=api_isolation.output_dir,
             )
             images = docker_registry.images
-            assert len(images["tags"]) == 1, f"{images} doesn't have the expected 1 tags, it has {len(images['tags'])}"
+            assert (
+                len(images["tags"]) == 1
+            ), f"{images} doesn't have the expected 1 tags, it has {len(images['tags'])}"
             assert image_info.get_target_complete_tag() in images["tags"][0]
 
         finally:
