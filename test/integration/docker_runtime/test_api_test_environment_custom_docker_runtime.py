24bcbed22297523f38e51ecc91f5c47c4e23a7c1
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2025-04-11 15:32:13 -0300

    #474: Converted test api test environment docker runtime to pytest

---
+++ test/integration/docker_runtime/test_api_test_environment_custom_docker_runtime.py
@@ -0,0 +1,37 @@
+import pytest
+
+from exasol_integration_test_docker_environment.test.get_test_container_content import get_test_container_content
+from exasol_integration_test_docker_environment.testing.api_test_environment import ApiTestEnvironment
+from exasol_integration_test_docker_environment.testing.api_test_environment_context_provider import \
+    build_api_context_provider_with_test_container
+from test.integration.docker_runtime.common import assert_container_runtime, default_docker_runtime
+
+
+
+@pytest.fixture(scope="module")
+def environment_with_custom_docker_runtime(
+    api_isolation_module: ApiTestEnvironment, default_docker_runtime
+):
+    provider = build_api_context_provider_with_test_container(
+        api_isolation_module, get_test_container_content()
+    )
+    additional_parameter = {
+        "docker_runtime": default_docker_runtime,
+    }
+    with provider(None, additional_parameter) as db:
+        yield db
+
+def test_test_container_runtime(environment_with_custom_docker_runtime, default_docker_runtime):
+    environment_info = environment_with_custom_docker_runtime.environment_info
+    test_container_name = environment_info.test_container_info.container_name
+    assert_container_runtime(test_container_name, default_docker_runtime)
+
+def test_database_container_runtime(environment_with_custom_docker_runtime, default_docker_runtime):
+    environment_info = environment_with_custom_docker_runtime.environment_info
+    database_container_name = (
+        environment_info.database_info.container_info.container_name
+    )
+    assert_container_runtime(
+        database_container_name, default_docker_runtime
+    )
+
---
+++ test/integration/docker_runtime/test_api_test_environment_invalid_docker_runtime.py
@@ -0,0 +1,25 @@
+from exasol_integration_test_docker_environment.lib.models import api_errors
+from exasol_integration_test_docker_environment.test.get_test_container_content import get_test_container_content
+from exasol_integration_test_docker_environment.testing import utils
+from exasol_integration_test_docker_environment.testing.api_test_environment import ApiTestEnvironment
+
+
+
+def test_docker_environment_not_available(api_isolation_module: ApiTestEnvironment):
+    exception_thrown = False
+    spawn_docker_test_environments_successful = False
+    try:
+        additional_parameters = {
+            "docker_runtime": "AAAABBBBCCCC_INVALID_RUNTIME_111122223333",
+        }
+        environment = api_isolation_module.spawn_docker_test_environment_with_test_container(
+            name=api_isolation_module.name,
+            test_container_content=get_test_container_content(),
+            additional_parameter=additional_parameters,
+        )
+        spawn_docker_test_environments_successful = True
+        utils.close_environments(environment)
+    except api_errors.TaskRuntimeError:
+        exception_thrown = True
+    assert not spawn_docker_test_environments_successful
+    assert exception_thrown
