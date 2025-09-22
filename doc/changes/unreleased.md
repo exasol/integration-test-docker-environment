bcbd8c3045b371c71b84a001a0c8f7ad7442272c
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2025-09-22 09:08:07 -0300

    #522: Convert integration API test to pytest - test_docker_access_method.py

--- doc/changes/unreleased.md
+++ doc/changes/unreleased.md
@@ -1,1 +1,4 @@
 # Unreleased
+
+## Refactorings
+ - #522: Convert integration API test to pytest - test_docker_access_method.py
--- test/unit/test_docker_access_method.py
+++ test/unit/test_docker_access_method.py
@@ -1,10 +1,5 @@
-"""
-Deprecated. Replaced by "./test/unit/test_docker_access_method.py"
-"""
-
 from typing import (
     Any,
-    Dict,
 )
 
 import luigi
@@ -17,33 +12,42 @@ from exasol_integration_test_docker_environment.lib.test_environment.parameter.d
     DbOsAccess,
     DockerDBTestEnvironmentParameter,
 )
+from exasol_integration_test_docker_environment.lib.base.run_task import (
+    generate_root_task,
+)
 
 
-class Testee(DependencyLoggerBaseTask, DockerDBTestEnvironmentParameter):
+class MockTask(DependencyLoggerBaseTask, DockerDBTestEnvironmentParameter):
+
     def run_task(self):
         pass
 
-    @classmethod
-    def make(cls, method: str) -> "Testee":
-        kwargs: dict[str, Any] = {"task_class": Testee}
+
+@pytest.fixture()
+def under_test_task_creator():
+
+    def make(method: DbOsAccess | None | str) -> MockTask:
+        kwargs: dict[str, Any] = {"task_class": MockTask}
         if method:
             kwargs["db_os_access"] = method
         task: Testee = generate_root_task(**kwargs)  # type: ignore
         luigi.build([task], workers=1, local_scheduler=True, log_level="INFO")
         return task
 
+    return make
 
-def test_db_os_access_default():
-    testee = Testee.make(None)
+
+def test_db_os_access_default(under_test_task_creator):
+    testee = under_test_task_creator(None)
     assert testee.db_os_access == DbOsAccess.DOCKER_EXEC
 
 
 @pytest.mark.parametrize("method", [DbOsAccess.DOCKER_EXEC, DbOsAccess.SSH])
-def test_db_os_access_parameter(method):
-    testee = Testee.make(method)
+def test_db_os_access_parameter(under_test_task_creator, method):
+    testee = under_test_task_creator(method)
     assert testee.db_os_access == method
 
 
-def test_db_os_access_invalid():
+def test_db_os_access_invalid(under_test_task_creator):
     with pytest.raises(AttributeError) as ex:
-        Testee.make("invalid method")
+        under_test_task_creator("invalid method")
