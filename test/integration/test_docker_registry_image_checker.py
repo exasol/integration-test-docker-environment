f2b1e53981dd50ec8fad952e2467c08a828e95ff
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2025-09-23 08:47:39 -0300

    #522: Converted integration API test to pytest - test_docker_registry_image_checker.py

---
+++ test/integration/test_docker_registry_image_checker.py
@@ -0,0 +1,19 @@
+import pytest
+from docker.errors import DockerException
+
+from exasol_integration_test_docker_environment.lib.docker.images.create.utils.docker_registry_image_checker import (
+    DockerRegistryImageChecker,
+)
+
+
+def test_pull_success():
+    image = "index.docker.io/registry:latest"
+    checker = DockerRegistryImageChecker()
+    exists = checker.check(image=image)
+    assert exists, f"Image {image} does not exist"
+
+def test_pull_fail_with_DockerException():
+    image = "index.docker.io/registry:abc"
+    checker = DockerRegistryImageChecker()
+    with pytest.raises(DockerException) as e:
+        checker.check(image=image)
