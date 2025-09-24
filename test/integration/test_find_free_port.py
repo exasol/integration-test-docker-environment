7aa18d0cd424ae13e054c0d1ca7cc6363b1825d9
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2025-09-24 13:08:11 -0300

    #522: Converted integration API test to pytest - test_find_free_port.py

---
+++ test/integration/test_find_free_port.py
@@ -0,0 +1,17 @@
+import unittest
+
+import pytest
+
+from exasol_integration_test_docker_environment.lib.test_environment.ports import (
+    find_free_ports,
+)
+
+
+@pytest.mark.parametrize("num_ports", [1, 2, 100, 1000])
+def test_find_ports(num_ports):
+    ports = find_free_ports(num_ports)
+    assert not 0 in ports
+    assert len(ports) == num_ports
+    # Check that there are no duplicates!
+    ports_set = set(ports)
+    assert len(ports) == len(ports_set)
