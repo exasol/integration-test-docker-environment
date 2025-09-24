e9107b7731ce7c66dc54537595a572002bdc194f
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2025-09-24 13:15:19 -0300

    #522: Converted integration API test to pytest - test_generate_graph_plot.py

---
+++ test/integration/base_task/test_generate_graph_plot.py
@@ -0,0 +1,47 @@
+import luigi
+
+from exasol_integration_test_docker_environment.lib.base.dependency_logger_base_task import (
+    DependencyLoggerBaseTask,
+)
+from exasol_integration_test_docker_environment.lib.base.run_task import (
+    generate_root_task,
+    run_task,
+)
+
+
+class TestTask(DependencyLoggerBaseTask):
+    x = luigi.Parameter()
+
+    def register_required(self):
+        self.register_dependency(
+            self.create_child_task(task_class=TestChildTask, y=["1", "2", "3"])
+        )
+
+    def run_task(self):
+        pass
+
+
+class TestChildTask(DependencyLoggerBaseTask):
+    y = luigi.ListParameter()
+
+    def run_task(self):
+        pass
+
+
+
+def test_generate_dependency_dot_file(tmp_path):
+    NUMBER_TASK = 5
+    task_id_generator = (x for x in range(NUMBER_TASK))
+
+    def create_task():
+        return generate_root_task(
+            task_class=TestTask, x=f"{next(task_id_generator)}"
+        )
+
+    for i in range(NUMBER_TASK):
+        dot_file = tmp_path / f"dot_file_{i}.dot"
+        run_task(
+            create_task, workers=5, task_dependencies_dot_file=str(dot_file)
+        )
+        assert dot_file.exists()
+
