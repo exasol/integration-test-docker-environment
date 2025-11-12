edb1b81ed044e6dda8fd4a14ac859e767ec5eca9
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2025-11-12 11:39:03 -0300

    Fixed formatting

--- test/unit/test_ports.py
+++ test/unit/test_ports.py
@@ -1,5 +1,6 @@
 from exasol_integration_test_docker_environment.lib.test_environment.ports import Ports
 
+
 def test_forward_ports():
     p = Ports.forward
     assert p.database == 8563
@@ -13,7 +14,7 @@ def test_default_ports():
     assert p.database == 8563
     assert p.bucketfs == 2580
     assert p.ssh == 22
-    assert p.bucketfs_https ==2581
+    assert p.bucketfs_https == 2581
 
 
 def test_external_ports():
@@ -21,4 +22,4 @@ def test_external_ports():
     assert p.database == 8563
     assert p.bucketfs == 2580
     assert p.ssh is None
-    assert p.bucketfs_https ==2581
+    assert p.bucketfs_https == 2581
