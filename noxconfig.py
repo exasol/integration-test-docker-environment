c94d45f4067594fc5d17f0886e6abbff4f9b593a
Ariel Schulz <ariel.schulz@exasol.com>
2025-12-15 11:31:59 +0100

    Update noxconfig to PTB 4.0.0 structure

--- noxconfig.py
+++ noxconfig.py
@@ -1,24 +1,26 @@
 from __future__ import annotations
 
-from collections.abc import Iterable
-from dataclasses import dataclass
 from pathlib import Path
 
-from nox import Session
+from exasol.toolbox.config import BaseConfig
+from pydantic import computed_field
 
 
-@dataclass(frozen=True)
-class Config:
-    root: Path = Path(__file__).parent
-    doc: Path = Path(__file__).parent / "doc"
-    source: Path = Path("exasol_integration_test_docker_environment")
-    version_file: Path = (
-        Path(__file__).parent
-        / "exasol_integration_test_docker_environment"
-        / "version.py"
-    )
-    path_filters: Iterable[str] = ("dist", ".eggs", "venv", "resources")
-    python_versions = ["3.10", "3.11", "3.12", "3.13"]
+class Config(BaseConfig):
+    @computed_field  # type: ignore[misc]
+    @property
+    def source_code_path(self) -> Path:
+        """
+        Path to the source code of the project.
+
+        In the ITDE, this needs to be overridden due to a custom directory setup.
+        This will be addressed in:
+            https://github.com/exasol/integration-test-docker-environment/issues/569
+        """
+        return self.root_path / self.project_name
 
 
-PROJECT_CONFIG = Config()
+PROJECT_CONFIG = Config(root_path=Path(__file__).parent,
+                            project_name="exasol_integration_test_docker_environment",
+                            python_versions=("3.10", "3.11", "3.12", "3.13"),
+                            add_to_excluded_python_paths=("resources",))
