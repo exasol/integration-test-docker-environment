4536627884a67e03487a00da62968fd97a0c65eb
Ariel Schulz <ariel.schulz@exasol.com>
2025-12-16 08:30:52 +0100

    Add comments to explain F401s

--- exasol_integration_test_docker_environment/main.py
+++ exasol_integration_test_docker_environment/main.py
@@ -4,7 +4,9 @@ from exasol_integration_test_docker_environment.cli.cli import cli
 
 
 def main():
-    # required so the cli will print the available subcommands
+    # The imports from `commands` are required so that `cli()` will print the available
+    # subcommands. Unfortunately, as these are unused imports within this file, an
+    # auto-formatting tool would want to remove them.
     from exasol_integration_test_docker_environment.cli.commands import (  # noqa: F401
         environment,
         health,
