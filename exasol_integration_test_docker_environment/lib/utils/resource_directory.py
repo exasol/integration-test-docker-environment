ae1c2f76af1212ac73e29b0948c678de2675a914
mibe <mikhail.beck@exasol.com>
2026-06-26 11:53:36 +0100

    Ran format:fix

--- exasol_integration_test_docker_environment/lib/utils/resource_directory.py
+++ exasol_integration_test_docker_environment/lib/utils/resource_directory.py
@@ -1,12 +1,13 @@
 import importlib.resources as ir
 import logging
-import tempfile
 import sys
+import tempfile
 
 if sys.version_info >= (3, 11):
     from importlib.resources.abc import Traversable
 else:
     from importlib.abc import Traversable
+
 from pathlib import Path
 from types import ModuleType
 
